b00d514022c08bdcc062d98f08052bcd34108df7
Dave Reed <dave.reed@tr.com>
2017-09-04 18:49:23 -0400

    initial move to create-react-app, dir structure is very different

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
-// import 'babel-polyfill'
-import React from 'react'
-import { render } from 'react-dom'
-import { browserHistory, hashHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
-import Root from './containers/Root'
-import configureStore from './store/configureStore'
-import createHistory from 'history/createBrowserHistory'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-
-import 'bootstrap/dist/css/bootstrap.css'
-
-import 'leaflet/dist/leaflet.css'
-
-// import '//unpkg.com/leaflet@1.0.3/dist/leaflet.cs'
-
-import './css/style.css'
-
-const store = configureStore()
-
-const history = createHistory()
-// const history = syncHistoryWithStore(hashHistory, store)
-
-const root = document.createElement('div')
-document.body.appendChild(root)
-
-render(
-  <Root store={store} history={history} />,
-  root
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
