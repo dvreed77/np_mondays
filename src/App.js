8591054673dcc917a7eda0219f81e00b7d15cd67
Dave Reed <dave.reed@tr.com>
2017-09-05 08:37:55 -0400

    have basic map working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MapGL from 'react-map-gl';
+
+const token = process.env.REACT_APP_MapboxAccessToken;
+
+console.log(process.env)
+
+// [-71.0589, 42.3601]
+class Dave extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                latitude: 42.3601,
+                longitude: -71.0589,
+                zoom: 14,
+                bearing: 0,
+                pitch: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        };
+    }
+
+    render() {
+
+        const {viewport} = this.state;
+
+        return (
+            <MapGL
+                {...viewport}
+                // mapStyle="mapbox://styles/mapbox/dark-v9"
+                mapStyle="mapbox://styles/dvreed77/cir1ar1fk000lbunsz4rsjc3f"
+                onViewportChange={v => this.setState({viewport: v})}
+                preventStyleDiffing={false}
+                mapboxApiAccessToken={token} >
+            </MapGL>
+        );
+    }
+
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App2">
+          <Dave />
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+// require('dotenv').config();
+
+// console.log(process.env)
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+console.log(process.env)
