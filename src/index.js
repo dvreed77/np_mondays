f57d733e69b839f63e421a21468c939f2cb38709
Dave Reed <dave.reed@tr.com>
2017-09-16 18:28:53 -0400

    fixed styling issues

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import 'mapbox-gl/dist/mapbox-gl.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
