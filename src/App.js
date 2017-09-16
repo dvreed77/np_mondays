9a837fb15c77ca84fd6a54a00736693a3791b835
Dave Reed <dave.reed@tr.com>
2017-09-16 17:02:51 -0400

    rendering SVG elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import MapGL from 'react-map-gl';
+// import MapGL from 'react-map-gl';
+import MapGL, {Marker, Popup, NavigationControl} from 'react-map-gl';
+import NPPin from './np-pin';
 
 const token = process.env.REACT_APP_MapboxAccessToken;
 
-console.log(process.env)
+console.log(process.env);
 
-// [-71.0589, 42.3601]
+
+
+const d = {
+    "source": {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-71.038887, 42.364506]
+                },
+                "properties": {
+                    "title": "Mapbox DC",
+                    "icon": "monument"
+                }
+            }]
+        }
+    }
+}
+
+
+
 class Dave extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            year: 2015,
+            data: null,
+            hoveredFeature: null,
             viewport: {
                 latitude: 42.3601,
                 longitude: -71.0589,
@@ -25,18 +53,77 @@ class Dave extends Component {
         };
     }
 
+    componentDidMount() {
+        // this._loadData();
+    }
+
+    _loadData = () => {
+
+        const data = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {"name": "Maine"},
+                    "geometry": {"type": "Point", "coordinates": [42.3601, -71.0589]}
+                }]
+    };
+
+
+        // updatePercentiles(data, f => f.properties.income[this.state.year]);
+
+        // const mapStyle = defaultMapStyle
+        //     .setIn(['sources', 'incomeByState'], {type: 'geojson', data});
+            // Add point layer to map
+            // .set('layers', defaultMapStyle.get('layers').push(dataLayer));
+
+        this.setState({data});
+    };
+
+    // _renderPopup() {
+    //     const {popupInfo} = this.state;
+    //
+    //     return popupInfo && (
+    //         <Popup tipSize={5}
+    //                anchor="top"
+    //                longitude={popupInfo.longitude}
+    //                latitude={popupInfo.latitude}
+    //                onClose={() => this.setState({popupInfo: null})} >
+    //             <div>
+    //                 Dave
+    //             </div>
+    //         </Popup>
+    //     );
+    // }
+
+    _renderCityMarker = (city, index) => {
+        return (
+            <Marker key={`marker-${index}`}
+                    longitude={city.longitude}
+                    latitude={city.latitude} >
+                <NPPin size={20} onClick={() => this.setState({popupInfo: city})} />
+            </Marker>
+        );
+    }
+
     render() {
 
         const {viewport} = this.state;
 
+        const CITIES = [
+            {latitude:42.3601, longitude:-71.0589}
+        ];
+
         return (
             <MapGL
                 {...viewport}
-                // mapStyle="mapbox://styles/mapbox/dark-v9"
                 mapStyle="mapbox://styles/dvreed77/cir1ar1fk000lbunsz4rsjc3f"
                 onViewportChange={v => this.setState({viewport: v})}
                 preventStyleDiffing={false}
                 mapboxApiAccessToken={token} >
+
+                { CITIES.map(this._renderCityMarker) }
+
             </MapGL>
         );
     }
