f68b239f69b210673eb41c8ba1ab00aaa7a40aee
Dave Reed <dave.reed@tr.com>
2017-09-16 18:53:13 -0400

    added locations.json.  Have a base MVP, that I can publish to website

--- src/np-info.js
+++ src/np-info.js
@@ -1,15 +1,21 @@
 import React, {PureComponent} from 'react';
+import moment from 'moment'
 
 export default class NPInfo extends PureComponent {
 
     render() {
+
         const {info} = this.props;
+
         const displayName = `${info.city}, ${info.state}`;
 
         return (
             <div>
-                <div>{info.url}</div>
-                <img width="240" src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/DenverCP.JPG/240px-DenverCP.JPG"/>
+                <div>
+                    {moment(info.date).format("MMM Do YYYY")}
+                    <a href={info.url} target="_blank">(link)</a>
+                </div>
+                <img width="240" src={info.img}/>
 
             </div>
         );
